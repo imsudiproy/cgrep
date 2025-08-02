0e01d01079c17f4660fc2afbfd980131fd6c6b06
Sudip Roy <sudiproy1999@gmail.com>
2025-08-02 17:48:19 +0530

    add simple argument passing logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    let pattern = std::env::args().nth(1).expect("No pattern provided");
+    let path = std::env::args().nth(2).expect("No Path provided");
+
+    println!("Pattern: {:?}, Path: {:?}", pattern, path);
 }
