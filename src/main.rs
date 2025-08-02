15211fd3bcf30129ec034f91a7536d9df2fb5fd3
Sudip Roy <sudiproy1999@gmail.com>
2025-08-02 18:26:44 +0530

    add clap implementation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
-fn main() {
-    let pattern = std::env::args().nth(1).expect("No pattern provided");
-    let path = std::env::args().nth(2).expect("No Path provided");
+use clap::Parser;
 
-    println!("Pattern: {:?}, Path: {:?}", pattern, path);
+#[derive(Parser)]
+struct Cli {
+    pattern : String,
+    path: std::path::PathBuf,
+}
+
+fn main() {
+    //implements how to take command line arguments without using any library
+    //like clap:
+    //====================================================================
+    //let pattern = std::env::args().nth(1).expect("No pattern provided");
+    //let path = std::env::args().nth(2).expect("No Path provided");
+    //====================================================================
+
+    let args = Cli::parse();
+
+    println!("Pattern: {:?}, Path: {:?}", args.pattern, args.path);
 }
