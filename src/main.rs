1d1fb157b13e3a6d911c1e384e9ab29f485972a8
Sudip Roy <sudiproy1999@gmail.com>
2025-08-03 23:16:49 +0530

    add error reporting

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,26 @@
+use anyhow::{Context, Result};
 use clap::Parser;
 
+/// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
 struct Cli {
-    pattern : String,
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
     path: std::path::PathBuf,
 }
 
-fn main() {
-    //implements how to take command line arguments without using any library
-    //like clap:
-    //====================================================================
-    //let pattern = std::env::args().nth(1).expect("No pattern provided");
-    //let path = std::env::args().nth(2).expect("No Path provided");
-    //====================================================================
-
+fn main() -> Result<()> {
     let args = Cli::parse();
-    let  content = std::fs::read_to_string(&args.path).expect("Couldn't read the file");
+
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("could not read file `{}`", args.path.display()))?;
 
     for line in content.lines() {
         if line.contains(&args.pattern) {
             println!("{}", line);
         }
     }
-    //println!("Pattern: {:?}, Path: {:?}", args.pattern, args.path);
+
+    Ok(())
 }
